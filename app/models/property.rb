92d004ee5d72682b9d5377f4f3677c83491a88ee
toyman640 <falakoadebayor@gmail.com>
2024-04-25 13:05:23 +0100

    Add validations to Property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
   belongs_to :property_type
   belongs_to :ownership_type
+  has_many_attached :images
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
